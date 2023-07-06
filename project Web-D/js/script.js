c588d2fdc443219417d083e294a32441dfa5fd6c
Sakshiii-Singhhh <105056983+Sakshiii-Singhhh@users.noreply.github.com>
2023-07-06 21:33:09 +0530

    Add files via upload

---
+++ project Web-D/js/script.js
@@ -0,0 +1,17 @@
+/*jshint esversion: 6 */
+//Mouseover event for the Order Now button in the home page
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByClassName("orderButton");
+    for (let orderButton of buttons) {
+        orderButton.addEventListener("mouseover", function () {
+            orderButton.textContent = "Great Choice!";
+        });
+        orderButton.addEventListener("click", function () {
+            alert("Your choice has been added to the cart!");
+        });
+        orderButton.addEventListener("mouseout", function () {
+            orderButton.textContent = "Order Now";
+        });
+    }
+});
+
